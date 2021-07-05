33fc595092cffd8a1d6e95ebdb383410500c34fe
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-07-05 16:01:07 -0700

    Implemented display of renter data in renter table

--- client/src/components/Renter.jsx
+++ client/src/components/Renter.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Renter = props => {
   return (
     <tr className="renter">
-      <td>Renter</td>
+      <td>{props.renterData.name}</td>
+      <td>{props.renterData.hourly_wages}</td>
+      <td>{props.renterData.hours_working}</td>
+      <td>{props.renterData.dog_count}</td>
+      <td>{props.renterData.cat_count}</td>
+      <td>{props.renterData.share}</td>
     </tr>
   );
 };
