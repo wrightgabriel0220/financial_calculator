8ff0292e89d35314b82c2bd218c4232748b7780f
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-08-11 18:00:58 -0700

    Implemented passage of form data from AddRenterModal to submit handler

--- client/src/components/modals/AddRenterModal.jsx
+++ client/src/components/modals/AddRenterModal.jsx
@@ -4,29 +4,45 @@ const AddRenterModal = props => {
   const submitHandler = event => {
     event.preventDefault();
 
-    props.closeModal();
+    const form = document.getElementById('modal');
+
+    if (form.checkValidity()) {
+      let formResults = {
+        name: document.getElementById('name-input').value,
+        hourlyWages: document.getElementById('hourly-rate-input').value,
+        hoursWorking: document.getElementById('hours-working-input').value,
+        dogCount: document.getElementById('dog-count-input').value || 0,
+        catCount: document.getElementById('cat-count-input').value || 0,
+        share: document.getElementById('share-input').value || 0
+      };
+
+      props.closeModal();
+      console.log(formResults);
+    } else {
+      form.reportValidity();
+    }
   }
 
   return (
     <form id="modal">
       <h2>Add Renter</h2>
       <label className="form-element"> Name:
-        <input type="text"></input>
+        <input id="name-input" required type="text"></input>
       </label>
       <label className="form-element"> Hourly Rate:
-        <input type="text"></input>
+        <input id="hourly-rate-input" required type="text"></input>
       </label>
       <label className="form-element"> Hours Working:
-        <input type="text"></input>
+        <input id="hours-working-input" required type="text"></input>
       </label>
       <label className="form-element"> # of Dogs:
-        <input type="text"></input>
+        <input id="dog-count-input" type="text"></input>
       </label>
       <label className="form-element"> # of Cats:
-        <input type="text"></input>
+        <input id="cat-count-input" type="text"></input>
       </label>
       <label className="form-element"> Share of Rent (decimal from 0 to 1):
-        <input type="text"></input>
+        <input id="share-input" type="text"></input>
       </label>
       <button onClick={submitHandler}>Submit</button>
     </form>
