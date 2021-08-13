d0e6ae49b3697841705d8c4188b2dbf1f5f7b8e2
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2021-08-13 03:09:02 -0700

    Implemented rent and move in cost estimations for Listings

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Listing = props => {
+  const [ renterShares, setRenterShares ] = useState({});
+
+  useEffect(() => {
+    let totalListingPrice = props.listingData.rent;
+    let renterShareTracker = {};
+    while (totalListingPrice > 0) {  
+      for (let renter of props.renters) {
+        renterShareTracker[renter.name] = renterShareTracker[renter.name] || 0;
+        let renterMax = Number(renter.hourly_wages) * renter.hours_working * 4.3333333 * .3 - 100;
+        if (renterShareTracker[renter.name] > 0 && renterShareTracker[renter.name] < renterMax) {
+          let overflowToRenter = (renterMax - renterShareTracker[renter.name] - totalListingPrice) > 0 ?
+            renterMax - renterShareTracker[renter.name] - totalListingPrice : 
+            renterMax - renterShareTracker[renter.name];
+          renterShareTracker[renter.name] += Math.round(overflowToRenter);
+          totalListingPrice -= overflowToRenter;
+        } else {
+          renterShareTracker[renter.name] += Math.round(getRenterShare(totalListingPrice, renter));
+          totalListingPrice -= Math.round(renterShareTracker[renter.name]);
+        }
+        if (totalListingPrice <= 0) {
+          break;
+        }
+      }
+    }
+    setRenterShares(renterShareTracker);
+  }, [])
+
   const getRenterShare = (rent, renter) => {
     let renterMax = renter.hourly_wages * renter.hours_working * 4.33333333 * .3 - 100;
     if (rent * (renter.share * .01) > renterMax) {
@@ -26,17 +53,21 @@ const Listing = props => {
         <thead>
           <tr>
             <td>Rent for this listing</td>
-            {props.renters.map(renter => <td>{renter.name}</td>)}
+            {props.renters.map(renter => <td key={renter.name}>{renter.name}</td>)}
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>{props.listingData.rent}</td>
-            {props.renters.map(renter => <td>{getRenterShare(props.listingData.rent, renter)}</td>)}
+            <td>${props.listingData.rent}</td>
+            {props.renters.map(renter => <td key={`share${renter.name}`}>${String(renterShares[renter.name])}</td>)}
           </tr>
           <tr>
-            <td>{props.renters.map(renter => getStartupCost(getRenterShare(props.listingData.rent, renter))).reduce((a, b) => a + b)}</td>
-            {props.renters.map(renter => <td>{getStartupCost(getRenterShare(props.listingData.rent, renter))}</td>)}
+            <td>Move-in Cost</td>
+            {props.renters.map(renter => <td key={`blank${renter.name}`}></td>)}
+          </tr>
+          <tr>
+            <td>{String(props.renters.map(renter => getStartupCost(renterShares[renter.name])).reduce((a, b) => a + b))}</td>
+            {props.renters.map(renter => <td key={`moveincost${renter.name}`}>${String(getStartupCost(renterShares[renter.name]))}</td>)}
           </tr>
         </tbody>
       </table>
